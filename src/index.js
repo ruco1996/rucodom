7e63e06cdd8a5116b0c96e3f13a31be9dcd14c03
ruco1996 <daniel_ruco@hotmail.es>
2020-07-09 10:23:27 +0200

    render component without children

--- src/index.js
+++ src/index.js
@@ -1,20 +1,56 @@
-/** @jsx d */
+/** @jsx daniDom */
 import "./styles.css";
 
 const root = document.getElementById("root");
 
-function d(type, props, ...children) {
+function daniDom(type, props, ...children) {
   return {
     type,
     props,
     children,
   };
 }
-const title = <h1>hello world</h1>;
+
+const Title = (
+  <h1 class="title" style="font-size: 50px;" data-test="hello">
+    dsdasda
+  </h1>
+);
+
+const MyComponent = (
+  <div>
+    <h1 class="title" style="font-size: 50px;" data-test="hello">
+      hello world
+    </h1>
+  </div>
+);
 
 function render(node) {
-  const element = document.createElement(node.type);
+  console.log(node);
+  var element;
+  if (typeof node.type == "string") {
+    element = document.createElement(node.type);
+    console.log(element);
+  }
+  if (typeof node.type == "object") {
+    return render(node.type);
+  }
+
+  if (node.props) {
+    Object.keys(node.props).forEach((key) => {
+      let att = document.createAttribute(key);
+      att.value = node.props[key];
+      element.setAttributeNode(att);
+    });
+  }
+
+  let text = document.createTextNode(node.children[0]);
+  element.appendChild(text);
   return element;
 }
 
-root.appendChild(render(title));
+// root.appendChild(render(<MyComponent hello="pim"></MyComponent>));
+
+root.appendChild(render(<Title />));
+
+// root.appendChild(render(title));
